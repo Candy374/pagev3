83760d90de0df2a97f0fd5f9fe91951d8e244ba7
ling.huang <lingling374@qq.com>
2017-01-24 22:23:47 +0800

    add build script

--- scripts/buildSite.sh
+++ scripts/buildSite.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
-SITE_DIRECTORY="site”
+SITE_DIRECTORY="site"
 
 GITHUB_REPO="https://github.com/Candy374/pagev3.git"
 
 GH_PAGES_SITE="https://candy374.github.io/pagev3/"
 
 
+# Setup repo if doesnt exist
+if [ ! -d "$SITE_DIRECTORY" ]; then
+  read -p "No site repo setup, can I create it at \"$PWD/$SITE_DIRECTORY\"? [Y/n] " -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]] && [[ ! $REPLY == "" ]]
+  then
+    echo "Exit by user"
+    exit 1
+  fi
+  git clone "$GITHUB_REPO" "$SITE_DIRECTORY"
+  cd "$SITE_DIRECTORY"
+  git checkout origin/gh-pages
+  git checkout -b gh-pages
+  git push --set-upstream origin gh-pages
+  cd ../
+fi
 
 
-
-Gulp dist
+gulp dist
 
 open dist/index.html
 
